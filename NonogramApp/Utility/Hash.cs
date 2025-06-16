8b8c0f1626b1afd6b2872973acd56cf5a0480145
JayBeitsma <s1214179@student.windesheim.nl>
2025-06-16 14:10:07 +0200

    added salt to make hash a bit stronger.

--- NonogramApp/Utility/Hash.cs
+++ NonogramApp/Utility/Hash.cs
@@ -10,12 +10,23 @@ namespace NonogramApp.Utility
     {
         public static uint PassHasher(string input)
         {
+            string salt = "C00K!3S";
             uint hash = 4629;
-            foreach (char c in input)
+            uint hash2 = 9320;
+            uint hash3;
+
+            foreach (char c in salt)
             {
                 hash = ((hash << 5) + hash) + c; 
             }
-            return hash;
+
+            foreach (char c in input)
+            {
+                hash2 = ((hash2 << 5) + hash2) + c;
+            }
+
+            hash3 = hash * hash2;
+            return hash3;
         }
     }
 }
